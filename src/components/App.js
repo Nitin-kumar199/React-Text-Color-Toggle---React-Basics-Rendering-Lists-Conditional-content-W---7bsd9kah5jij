5998b41f03eb9243703e78b928ea137587f8ab1b
Nitin-kumar199 <112743746+Nitin-kumar199@users.noreply.github.com>
2023-02-13 16:36:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  useEffect(() => {
+    const pTag = document.querySelector('#main p');
+    if (isClicked) {
+      pTag.style.color = 'blue';
+      pTag.style.fontSize = '30px';
+      pTag.style.fontStyle = 'italic';
+    } else {
+      pTag.style.color = 'red';
+      pTag.style.fontSize = '30px';
+      pTag.style.fontStyle = 'normal';
+    }
+  }, [isClicked]);
+
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className="title">Newton School</p>
+      <button id='button' onClick={handleClick}>Change Style</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
